1b23dcaf1ac0d3e1a4aa7406f437129c02ef818f
veratech <jared@veratech.co.nz>
2014-12-14 18:31:43 +1300

    first

---
+++ custom.js
@@ -0,0 +1,64 @@
+// Custom JS
+
+$(function(){
+
+
+	//Setup
+	var $list, $newItemForm, $newItemButton;
+	var item = '';
+	$list = $('ul');
+	$newItemForm = $('#newItemForm');
+	$newItemButton = $('#newItemButton');
+
+	$('li').hide().each(function(index){
+		$(this).delay(450 * index).fadeIn(1600);
+	});
+
+
+	//Item Counter
+	function updateCount(){
+		var items = $('li [class!=complete]').length;
+		$('#counter').text(items);
+	}
+	updateCount();
+
+	//Setup the form for new items
+	$newItemButton.show();
+	$newItemForm.hide();
+	$('#showForm').on('click', function() {
+		$newItemButton.hide();
+		$newItemForm.show();
+	});
+
+	//Adding a new list item\
+	$newItemForm.on('submit', function(e) {
+		e.preventDefault();
+		var text = $('input:text').val();
+		$list.append('<li>' + text + '</li>');
+		$('input:text').val('');
+		updateCount();
+	});
+
+	//Click Handling - Uses Delegation on <ul> Element
+	$list.on('click', function(){
+		var $this = $(this);
+		var complete = $this.hasClass('complete');
+
+		if(complete === true) {
+			$this.animate({
+				opacity:0.0,
+				paddingLeft: '+-180'
+			}, 500, 'swing', function() {
+				$this.remove();
+			});
+			} else {
+				item = $this.text();
+				$this.remove();
+				$list
+				.append('<li class=\"complete\">' + item + '</li>')
+				.hide().fadeIn(300);
+				updateCount();
+
+			}
+	});
+});
